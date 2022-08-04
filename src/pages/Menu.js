eadcca3d1d3ead673e3c3529209f973527bf6c5f
Ying Ye <yingwilliams1406@outlook.com>
2022-08-04 12:55:04 -0400

    Revert "rendered menu on the menu page"
    This reverts commit 72c102157424e4ea100b5a2f33023ccc3b6a974a.

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,73 +1,51 @@
 import React from "react";
-import { db } from "../firebase-config";
-import { collection, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
+// import { db } from "./firebase-config";
+// import { collection, getDocs } from "firebase/firestore";
+// import { useEffect, useState } from "react";
 
-function Menu() {
-  const appetizerCollectionRef = collection(db, "appetizers");
-  const jiangNanCollectionRef = collection(db, "jiangnan");
-  const [appetizerList, setAppetizerList] = useState([]);
-  const [jiangNanList, setJiangNanList] = useState([]);
+function Menu({ appetizers }) {
+  // const appetizerCollectionRef = collection(db, "appetizer");
+  // const [appetizers, setAppetizers] = useState([]);
 
-  useEffect(() => {
-    const getAppetizerList = async () => {
-      const appetizersData = await getDocs(appetizerCollectionRef);
-      setAppetizerList(
-        appetizersData.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }))
+  // const getMenuAppetizer = async () => {
+  //   const appetizersData = await getDocs(appetizerCollectionRef);
+  //   setAppetizers(
+  //     appetizersData.docs.map((doc) => ({
+  //       ...doc.data(),
+  //       id: doc.id,
+  //     }))
+  //   );
+
+  //   // console.log("*************APPTIZERS*************************");
+  //   // console.log(appetizers);
+  // };
+
+  // useEffect(() => {
+  //   getMenuAppetizer();
+  // }, []);
+  const allAppet = () => {
+    appetizers.map((appet) => {
+      return (
+        <li>
+          {appet.item} ${appet.price}
+          {/* console.log({appet.item}) */}
+        </li>
       );
-    };
-
-    const getJiangNanList = async () => {
-      const jiangNanData = await getDocs(jiangNanCollectionRef);
-      setJiangNanList(
-        jiangNanData.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }))
-      );
-    };
-    getAppetizerList();
-    getJiangNanList();
-    // console.log("*************APPTIZERS*************************");
-    // console.log(appetizerList);
-  }, []);
-
+    });
+  };
   return (
     <div>
-      <div className="appet">
-        <div>
-          <h1>appetizer</h1>
-        </div>
-        <div>
-          {appetizerList.map((appet) => {
-            return (
-              <div className="menuContainer">
-                <div className="itemList">{appet.item}</div>
-                <div className="itemPrice">${appet.price}</div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-
-      <div className="jiangNan">
-        <div>
-          <h1>Jiang Nan Reminisce</h1>
-        </div>
-        <div>
-          {jiangNanList.map((jiangNanFood) => {
-            return (
-              <div className="menuContainer">
-                <div className="itemList">{jiangNanFood.item}</div>
-                <div className="itemPrice">${jiangNanFood.price}</div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+      <h2>APPTIZERS</h2>
+      <ul>
+        {/* {allAppet.map((appet) => {
+          return (
+            <li>
+              {appet.item} ${appet.price}
+            </li>
+          );
+        })} */}
+        {allAppet}
+      </ul>
     </div>
   );
 }
