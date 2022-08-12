26ae991d74cd536a46c3e814cc37ded49fe6af99
Ying Ye <yingwilliams1406@outlook.com>
2022-08-12 12:25:33 -0400

    added opetions for payment with no fuction yet

--- src/pages/Bill.js
+++ src/pages/Bill.js
@@ -31,6 +31,17 @@ function Bill({ tableNum, peopleNum, ordersInBill, likeButton, likeFood }) {
   const Tax = Number(Number.parseFloat(finalTotal * 0.08).toFixed(2));
   const Total = Number(Number.parseFloat(finalTotal + Tax).toFixed(2));
 
+  const paymentOption = [
+    { value: "", text: "...Choose an option" },
+    { value: "As it is", text: "As it is" },
+    { value: "Split by num of peoples", text: "Split by num of peoples" },
+    { value: "Split in my own way", text: "Split in my own way" },
+  ];
+
+  const selectPayment = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>Table #: {tableNum}</h1>
@@ -40,6 +51,21 @@ function Bill({ tableNum, peopleNum, ordersInBill, likeButton, likeFood }) {
         <h2>Sub Total: ${finalTotal}</h2>
         <h2>Tax: ${Tax}</h2>
         <h2>Total: ${Total}</h2>
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div>
+        <h2>How would you like to pay your bill?</h2>
+        <select onChange={selectPayment} name="paymentOptions" id="pmt-select">
+          {paymentOption.map((option, index) => {
+            return (
+              <option key={index} value={option.value}>
+                {option.text}
+              </option>
+            );
+          })}
+        </select>
       </div>
     </div>
   );
